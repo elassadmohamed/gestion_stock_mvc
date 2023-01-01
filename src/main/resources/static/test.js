5f8226226f11daf2941b0ba0ccbeb5f7c0db09c4
elassadmohamed <34839914+elassadmohamed@users.noreply.github.com>
2023-01-01 20:00:15 +0100

    Update test.js

--- src/main/resources/static/test.js
+++ src/main/resources/static/test.js
@@ -1,1 +1,26 @@
-console.log("Bojnour github");
+console.log("Bojnour1 github");
+console.log("Bojnour2 github");
+console.log("Bojnour3 github");
+
+
+
+
+
+
+console.log("Bojnour4 github");
+console.log("Bojnour5 github");
+console.log("Bojnour6 github");
+
+
+
+
+
+
+
+
+
+
+
+console.log("Bojnour7 github");
+console.log("Bojnour8 github");
+console.log("Bojnour9 github");
